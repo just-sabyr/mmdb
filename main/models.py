b6e2070d2b91c54c2979b817c7eb19e4d40efaaf
just-sabyr <bazarymbetovsabyr@gmail.com>
2024-02-20 20:34:56 +0300

    added translation for movie titles in movies_list.html, model field translation was used

--- main/models.py
+++ main/models.py
@@ -1,13 +1,38 @@
 from django.db import models
 
+from django.utils.translation import get_language
 
 class MovieDetails(models.Model):
     title = models.CharField(max_length=500)
+    title_fr = models.CharField(max_length=500)
+
     description = models.TextField()
+    description_fr = models.TextField()
+
     stars = models.PositiveSmallIntegerField()
 
     def __str__(self):
         return self.title
+    
+    def get_title(self):
+        return self._get_translated_field('title')
+    
+    def get_description(self):
+        return self._get_translated_field('description')
+    
+    def _get_translated_field(self, field_name):
+        original_field_name = field_name
+
+        lang_code = get_language()
+
+        if lang_code != 'en':
+            field_name = '{}_{}'.format(field_name, lang_code)
+        field_value = getattr(self, field_name)
+
+        if field_value:
+            return field_value
+        else:
+            return getattr(self, original_field_name)
 
 
 class MovieReview(models.Model):
